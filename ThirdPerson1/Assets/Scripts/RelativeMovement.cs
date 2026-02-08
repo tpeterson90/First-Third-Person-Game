cb672ffc8d3cbe4d167ef80116b629bb869069ed
Tasha Peterson <tpeterson1026@gmail.com>
2026-02-07 19:48:24 -0800

    Adding Animation
    Currently working through animation.
    
    Need to fix: Noticed player is not walking smooth.

--- ThirdPerson1/Assets/Scripts/RelativeMovement.cs
+++ ThirdPerson1/Assets/Scripts/RelativeMovement.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+
 public class RelativeMovement : MonoBehaviour
 {
     [SerializeField] Transform target;
@@ -14,6 +16,7 @@ public class RelativeMovement : MonoBehaviour
     public float minFall = -1.5f;
     private float vertSpeed;
     private CharacterController charController;
+    private ControllerColliderHit contact;
 
     void Start()
     {
@@ -23,11 +26,20 @@ public class RelativeMovement : MonoBehaviour
 
     void Update()
     {
+        bool hitGround = false;
+        RaycastHit hit;
         Vector3 movement = Vector3.zero;
         float horInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
+        Vector3 origin = transform.position + Vector3.up * 0.1f;
 
-        if (horInput !=0 || vertInput != 0)
+        if (vertSpeed < 0 && Physics.Raycast(origin, Vector3.down, out hit))
+        {
+            float check = (charController.height + charController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+
+        if (horInput != 0 || vertInput != 0)
         {
             Vector3 right = target.right;
             Vector3 forward = Vector3.Cross(right, Vector3.up);
@@ -40,7 +52,7 @@ public class RelativeMovement : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
 
-        if (charController.isGrounded)
+        if (hitGround)
         {
             if (Input.GetButtonDown("Jump"))
             {
@@ -56,8 +68,23 @@ public class RelativeMovement : MonoBehaviour
             vertSpeed += gravity * 5 * Time.deltaTime;
             if (vertSpeed < terminalVelocity) vertSpeed = terminalVelocity;
         }
+        //*
+        if (charController.isGrounded)
+        {
+            if (Vector3.Dot(movement, contact.normal) < 0)
+            {
+                movement = contact.normal * moveSpeed;
+            }
+        }
+        //*
+
         movement.y = vertSpeed;
         movement *= Time.deltaTime;
         charController.Move(movement);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contact = hit;
     }
 }
