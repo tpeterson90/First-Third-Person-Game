a075a59109e4b899b65c9abd62cfe390c1cac4d8
Tasha Peterson <tpeterson1026@gmail.com>
2026-02-07 18:19:51 -0800

    Player can jump

--- ThirdPerson1/Assets/Scripts/RelativeMovement.cs
+++ ThirdPerson1/Assets/Scripts/RelativeMovement.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class RelativeMovement : MonoBehaviour
 {
     [SerializeField] Transform target;
+    public float jumpSpeed = 15f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10f;
     public float rotSpeed = 15f;
     public float moveSpeed = 6f;
+    public float minFall = -1.5f;
+    private float vertSpeed;
     private CharacterController charController;
 
     void Start()
     {
+        vertSpeed = minFall;
         charController = GetComponent<CharacterController>();
     }
 
@@ -20,16 +26,37 @@ public class RelativeMovement : MonoBehaviour
         Vector3 movement = Vector3.zero;
         float horInput = Input.GetAxis("Horizontal");
         float vertInput = Input.GetAxis("Vertical");
+
         if (horInput !=0 || vertInput != 0)
         {
             Vector3 right = target.right;
             Vector3 forward = Vector3.Cross(right, Vector3.up);
+
             movement = (right * horInput) + (forward * vertInput);
             movement *= moveSpeed;
             movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
             Quaternion direction = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
+
+        if (charController.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                vertSpeed = minFall;
+            }
+        }
+        else
+        {
+            vertSpeed += gravity * 5 * Time.deltaTime;
+            if (vertSpeed < terminalVelocity) vertSpeed = terminalVelocity;
+        }
+        movement.y = vertSpeed;
         movement *= Time.deltaTime;
         charController.Move(movement);
     }
